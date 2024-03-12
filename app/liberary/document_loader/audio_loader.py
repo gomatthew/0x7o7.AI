baf2104098bbd249a68068cdd45da80e3baeae5d
0x7o7 <0x7o7@0x7o7s-MacBook-Pro.local>
2024-03-12 22:23:22 +0800

    add start model logic

---
+++ app/liberary/document_loader/audio_loader.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from langchain_community.document_loaders.parsers.audio import OpenAIWhisperParser
---
+++ app/liberary/model_worker/manage.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from fastchat.serve.model_worker import app, GptqConfig, AWQConfig, ModelWorker
+
+
+def run_model():
+    gptq_config = GptqConfig(ckpt=args.gptq_ckpt or args.model_path,
+                             wbits=args.gptq_wbits,
+                             groupsize=args.gptq_groupsize,
+                             act_order=args.gptq_act_order, )
+    awq_config = AWQConfig(
+        ckpt=args.awq_ckpt or args.model_path,
+        wbits=args.awq_wbits,
+        groupsize=args.awq_groupsize, )
+    worker = ModelWorker(                controller_addr=args.controller_address,
+                worker_addr=args.worker_address,
+                worker_id=worker_id,
+                model_path=args.model_path,
+                model_names=args.model_names,
+                limit_worker_concurrency=args.limit_worker_concurrency,
+                no_register=args.no_register,
+                device=args.device,
+                num_gpus=args.num_gpus,
+                max_gpu_memory=args.max_gpu_memory,
+                load_8bit=args.load_8bit,
+                cpu_offloading=args.cpu_offloading,
+                gptq_config=gptq_config,
+                awq_config=awq_config,
+                stream_interval=args.stream_interval,
+                conv_template=args.conv_template,
+                embed_in_truncate=args.embed_in_truncate,)
+    app._worker = worker
+    return app
