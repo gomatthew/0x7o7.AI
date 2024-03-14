a2ab7739c51eaedab42e88faa14d0aa2234e57f6
0x7o7 <0x7o7@0x7o7s-MacBook-Pro.local>
2024-03-14 23:46:15 +0800

    fix pep8

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from app.controller import test_router
 from app.configs import get_config_by_env
 from fastapi.middleware.cors import CORSMiddleware
 
+
 def create_app(env):
     config = get_config_by_env(env)
     _app = FastAPI(
@@ -21,5 +22,3 @@ def create_app(env):
     _app.include_router(test_router)
     _app.__setattr__("config", config)
     return _app
-
-
