930d8f05ec9f565e7737c9e4076e51dca4f6b52f
0x7o7 <majian@sup-info.com.cn>
2024-04-28 14:30:57 +0800

    finish text chat logic

--- app/service/chat_service/chat_bot.py
+++ app/service/chat_service/chat_bot.py
@@ -2,15 +2,19 @@
 import asyncio
 import json
 from fastapi import Request, Body
-from app import app
-from app.utils import wrap_done
-from app.dto.response_dto import BaseResponse
 from sse_starlette.sse import EventSourceResponse
-from app.service.model_service.model_service import get_hugging_face_llm
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.callbacks import AsyncIteratorCallbackHandler
+from app import app
+from app.utils import wrap_done
+from app.service.model_service.model_service import get_hugging_face_llm
 from app.liberary.prompts.prompt_template import PROMPT_TEMPLATE
+from app.service.chat_service.utils import make_chat_history_by_user_id
+from app.liberary.enum.chat_type_enum import ChatTypeEnum
+from app.liberary.db.repository.message_repository import add_message_to_db
+from app.liberary.db.repository.conversation_repository import add_conversation_to_db
+from app.liberary.callback_handler.conversation_callback_handler import ConversationCallbackHandler
 
 
 async def chat_bot(request: Request,
@@ -18,24 +22,36 @@ async def chat_bot(request: Request,
                    conversation_id: str = Body('', description='Conversation Id'),
                    prompt_template: str = Body('default', description='prompt_template'),
                    model_name: str = Body(app.config.get('LLM_MODELS')[0], description='LLM Model Name'),
+                   memory: bool = Body(False, description='need memory'),
                    # temperature: float = Body(app.config.get('TEMPERATURE'), description='Model Temperature'),
                    stream: bool = Body(True, description='Stream Output')):
+    chat_history = None
+    if not conversation_id:
+        conversation_id = add_conversation_to_db(user_id='user_id', user_query=query)
+    message_id = add_message_to_db(message_type=ChatTypeEnum.TEXT_CHAT.name, conversation_id=conversation_id,
+                                   user_id='user_id', user_query=query, create_user='0x7o7')
     llm = get_hugging_face_llm(model_name)
     custom_prompt = PROMPT_TEMPLATE.get(prompt_template, 'default')
     prompt = PromptTemplate.from_template(custom_prompt)
-    chain = LLMChain(llm=llm, promtp=prompt)
-    callback = AsyncIteratorCallbackHandler()
+    if memory:
+        chat_history = make_chat_history_by_user_id(user_id='user_id', message_type=ChatTypeEnum.TEXT_CHAT.name,
+                                                    history_len=3)
+    chain = LLMChain(llm=llm, promtp=prompt, memory=chat_history)
+    async_callback = AsyncIteratorCallbackHandler()
+    custom_callback = ConversationCallbackHandler(conversation_id=conversation_id, message_id=message_id,
+                                                  chat_type=ChatTypeEnum.TEXT_CHAT.name, query=query)
+    callbacks = [async_callback, custom_callback]
 
     async def chat_iterator():
         task = asyncio.create_task((wrap_done(
-            chain.acall({"input": query}),
-            callback.done)))
+            chain.acall({"input": query}, callbacks=callbacks),
+            async_callback.done)))
         if stream:
-            async for token in callback.aiter():
+            async for token in async_callback.aiter():
                 yield json.dumps({'status': 200, 'message': 'success', 'data': {'token': token}})
         else:
             answer = ''
-            async for token in callback.aiter():
+            async for token in async_callback.aiter():
                 answer += token
             yield json.dumps({'status': 200, 'message': 'success', 'data': {'token': answer}})
         await task
--- app/service/chat_service/utils.py
+++ app/service/chat_service/utils.py
@@ -3,13 +3,22 @@ from typing import List
 from langchain.memory import ChatMessageHistory
 from app.dto.chat_dto import ChatHistory
 from app.liberary.enum.chat_enum import ChatRoleEnum
+from app.liberary.db.repository.message_repository import get_chat_history_by_user_id
 
 
+def make_chat_history_by_user_id(user_id: str, message_type, history_len):
+    db_query = get_chat_history_by_user_id(user_id=user_id, message_type=message_type, history_len=history_len)
+    chat_history_list = list()
+    for chat in db_query:
+        chat_history_list.append(ChatHistory(role="USER", content=chat[0]))
+        chat_history_list.append(ChatHistory(role="AI", content=chat[1]))
+    return make_chat_history(chat_history_list)
 
-def make_chat_history(chat_history: List[ChatHistory], history_len: int = 3):
+
+def make_chat_history(chat_history: List[ChatHistory]):
     __chat_history__ = ChatMessageHistory()
-    history_len = history_len * 2
-    chat_history = chat_history[-history_len:]
+    # history_len = history_len * 2
+    # chat_history = chat_history[-history_len:]
     for history in chat_history:
         match history.role:
             case ChatRoleEnum.AI.name:
